1a002675454b33fd8c46945d277c518e9c89a045
gcarniglia <gcarniglia@fi.uba.ar>
2026-08-30 00:54:53 -0300

    feat(ej-5): [WIP] transport layer for server

---
+++ services/server/src/internal/transport/safe_socket.py
@@ -0,0 +1,43 @@
+import socket
+
+from services.server.src import safe_socket
+
+
+class Socket7E:
+
+    MAGIC_NUMBER = b'\x7E'
+
+    def __init__(self, socket: socket.socket):
+        self.__socket = socket
+
+    ''' Recibe la cantidad especificada de bytes del socket'''
+    def __recv(self, size: int) -> bytes:
+        return safe_socket.recv_all(self.__socket, size)
+
+    ''' Envía todos los bytes especificados al socket'''
+    def __send(self, bytes: bytes) -> int:
+        return safe_socket.send_all(self.__socket, bytes)
+
+    ''' Devuelve en bytes la longitud que tiene el payload del body'''
+    def read_header(self) -> bytes:
+        while True:
+            byte = self.__recv(1)
+            if byte == self.MAGIC_NUMBER:
+                break
+        header_size_bytes = self.__recv(1)
+        header_fields = self.__recv(header_size_bytes[0])
+        return header_fields
+
+    def read_payload(self, size: int) -> bytes:
+        return self.__recv(size)
+    
+    def send_header(self, bytes: bytes) -> int:
+        self.__send(self.MAGIC_NUMBER)
+        return self.__send(bytes)
+
+    def send_payload(self, bytes: bytes) -> int:
+        return self.__send(bytes)
+
+    ''' Cierra la conexión TCP con el cliente'''
+    def close(self) -> None:
+        self.__socket.close()
